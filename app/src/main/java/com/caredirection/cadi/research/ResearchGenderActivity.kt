677d565fa1ff97cd187a54a5135071ca899d726c
Haeeul <etoilune00@gmail.com>
2020-07-30 05:45:51 +0900

    [refactor] ResearchGenderActivity.kt 수정

--- app/src/main/java/com/caredirection/cadi/research/ResearchGenderActivity.kt
+++ app/src/main/java/com/caredirection/cadi/research/ResearchGenderActivity.kt
@@ -1,7 +1,5 @@
 package com.caredirection.cadi.research
 
-import android.app.DatePickerDialog
-import android.app.TimePickerDialog
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -10,8 +8,6 @@ import android.view.WindowManager
 import android.widget.Toast
 import com.caredirection.cadi.R
 import kotlinx.android.synthetic.main.activity_research_gender.*
-import kotlinx.android.synthetic.main.activity_research_name.*
-import java.util.*
 
 class ResearchGenderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,16 +16,26 @@ class ResearchGenderActivity : AppCompatActivity() {
 
         setStatusBarTransparent()
 
-        makeController()
+        initTitle()
+
+        makeListener()
     }
 
-    // 사용자 입력 확인
-    private fun makeController() {
-        val intent = getIntent()
+    private fun initTitle(){
         val nick = intent.getStringExtra("nick")
 
-        txt_gender_title.text = nick + "님의\n건강기능식품 선택을 도와드릴게요"
+        txt_genderTitle.text = nick + "님의\n건강기능식품 선택을 도와드릴게요"
+    }
 
+    // 버튼 클릭리스너 지정
+    private fun makeListener(){
+        setGenderClickListener()
+        setYearClickListener()
+        setBackClickListener()
+        setNextClickListener()
+    }
+
+    private fun setGenderClickListener(){
         btn_women?.setOnClickListener {
             btn_women?.isChecked = true
             btn_man?.isChecked = false
@@ -41,35 +47,41 @@ class ResearchGenderActivity : AppCompatActivity() {
             btn_man?.isChecked = true
             checkNextButton()
         }
+    }
 
+    private fun setYearClickListener(){
         btn_year?.setOnClickListener {
             btn_year?.isChecked = true
             checkNextButton()
             Toast.makeText(this,"아직 안했지롱", Toast.LENGTH_SHORT).show()
         }
+    }
 
-        btn_gender_back?.setOnClickListener {
+    private fun setBackClickListener(){
+        btn_genderBack?.setOnClickListener {
             finish()
         }
+    }
 
-        btn_gender_next?.setOnClickListener {
-            val disease_intent = Intent(this, ResearchDiseaseActivity::class.java)
+    private fun setNextClickListener() {
+        btn_genderNext?.setOnClickListener {
+            val diseaseIntent = Intent(this, ResearchDiseaseActivity::class.java)
 
-            startActivity(disease_intent)
+            startActivity(diseaseIntent)
         }
     }
 
     // 다음 버튼 처리를 위한 확인
     private fun checkNextButton(){
-        btn_gender_next.isEnabled = (btn_women.isChecked || btn_man.isChecked) && btn_year.isChecked
-        if(btn_gender_next.isEnabled) btn_gender_next.setTextColor(resources.getColor(R.color.colorWhite))
-        else btn_gender_next.setTextColor(resources.getColor(R.color.colorDarkGray))
+        btn_genderNext.isEnabled = (btn_women.isChecked || btn_man.isChecked) && btn_year.isChecked
+        if(btn_genderNext.isEnabled) btn_genderNext.setTextColor(resources.getColor(R.color.colorWhite))
+        else btn_genderNext.setTextColor(resources.getColor(R.color.colorDarkGray))
     }
 
     // 상태바 투명 설정
     private fun setStatusBarTransparent(){
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
-        cl_research_gender.setPadding(0, getStatusBarHeight(this), 0, 0)
+        cl_researchGender.setPadding(0, getStatusBarHeight(this), 0, 0)
     }
 
     // 상태바 높이 정보
