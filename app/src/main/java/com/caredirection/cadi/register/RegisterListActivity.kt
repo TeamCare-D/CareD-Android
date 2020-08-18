e46b2fbd60092cf0244f1d8dc538d7508a23ab91
Haeeul <etoilune00@gmail.com>
2020-08-19 03:42:21 +0900

    [feat] 복용제품등록 첫화면 레이아웃 작업

---
+++ app/src/main/java/com/caredirection/cadi/register/RegisterListActivity.kt
@@ -0,0 +1,12 @@
+package com.caredirection.cadi.register
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.caredirection.cadi.R
+
+class RegisterListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_list)
+    }
+}
