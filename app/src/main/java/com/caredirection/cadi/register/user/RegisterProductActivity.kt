fb748e099c2f162ec52da3994a095d23f0504363
Haeeul <etoilune00@gmail.com>
2020-08-20 04:39:44 +0900

    [feat] 나만의 제품 등록뷰 레이아웃 구현

---
+++ app/src/main/java/com/caredirection/cadi/register/user/RegisterProductActivity.kt
@@ -0,0 +1,12 @@
+package com.caredirection.cadi.register.user
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.caredirection.cadi.R
+
+class RegisterProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_product)
+    }
+}
