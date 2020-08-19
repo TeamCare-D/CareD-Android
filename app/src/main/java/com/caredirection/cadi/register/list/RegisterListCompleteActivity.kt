92c8a26a1996559f6448ad18f32472e141c5410b
Haeeul <etoilune00@gmail.com>
2020-08-20 03:22:49 +0900

    [feat] 복용제품등록 완료뷰 레이아웃 구현

---
+++ app/src/main/java/com/caredirection/cadi/register/list/RegisterListCompleteActivity.kt
@@ -0,0 +1,12 @@
+package com.caredirection.cadi.register.list
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.caredirection.cadi.R
+
+class RegisterListCompleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_list_complete)
+    }
+}
