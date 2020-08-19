5e589d5398316675aa5713cadf5645d316f0fe70
Haeeul <etoilune00@gmail.com>
2020-08-20 05:08:54 +0900

    [style] 복용제품등록 상태바 투명 설정

--- app/src/main/java/com/caredirection/cadi/register/list/RegisterListCompleteActivity.kt
+++ app/src/main/java/com/caredirection/cadi/register/list/RegisterListCompleteActivity.kt
@@ -1,12 +1,31 @@
 package com.caredirection.cadi.register.list
 
+import android.content.Context
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.caredirection.cadi.R
+import kotlinx.android.synthetic.main.activity_register_list_complete.*
 
 class RegisterListCompleteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_list_complete)
+
+        setStatusBarTransparent()
+    }
+
+    // 상태바 투명 설정
+    private fun setStatusBarTransparent(){
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        cl_register_list_complete.setPadding(0, getStatusBarHeight(this), 0, 0)
+    }
+
+    // 상태바 높이 정보
+    private fun getStatusBarHeight(context: Context): Int {
+        val resourceId = context.resources.getIdentifier("status_bar_height", "dimen", "android")
+
+        return if (resourceId > 0) context.resources.getDimensionPixelSize(resourceId)
+        else 0
     }
 }
