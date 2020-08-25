92fd8b2fa31d9edcf058189f31aca9e20765fed5
Choong <cndqjacndqja@naver.com>
2020-08-26 00:08:16 +0900

    [feat] 제품 필터 추가

--- app/src/main/java/com/caredirection/cadi/product/detail/adapter/RvCategoryAdapter.kt
+++ app/src/main/java/com/caredirection/cadi/product/detail/adapter/RvCategoryAdapter.kt
@@ -4,7 +4,6 @@ import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.inflate
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
