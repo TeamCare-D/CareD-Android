8070e69902dc665421651cd6fcf3d368da72a8eb
Choong <cndqjacndqja@naver.com>
2020-08-28 02:09:44 +0900

    [feat] retrofit2 셋업 추가

--- app/src/main/java/com/caredirection/cadi/network/RequestInterface.kt
+++ app/src/main/java/com/caredirection/cadi/network/RequestInterface.kt
@@ -1,4 +1,17 @@
 package com.caredirection.cadi.network
 
+import com.caredirection.cadi.networkdata.GraphIngredient
+import com.caredirection.cadi.networkdata.GraphIngredientList
+import retrofit2.Call
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Header
+import retrofit2.http.POST
+
 interface RequestInterface {
+
+    @FormUrlEncoded
+    @POST("/graph/vitamin-mineral")
+    fun getGraphVitamin(
+        @Header("token")token : String
+    ) : Call<GraphIngredientList>
 }
--- app/src/main/java/com/caredirection/cadi/networkdata/GraphIngredient.kt
+++ app/src/main/java/com/caredirection/cadi/networkdata/GraphIngredient.kt
@@ -1,6 +1,12 @@
 package com.caredirection.cadi.networkdata
 
-data class GrapgIngredient(
+data class GraphIngredientList(
+    val status: Int,
+    val message: String,
+    val data: List<GraphIngredient>
+)
+
+data class GraphIngredient(
     val ingredient_idx: Int,
     val ingredient_name: String,
     val ingredient_percentage: Int
