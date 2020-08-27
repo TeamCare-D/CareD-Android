b663c34d0e5a757bfa15cb37e9f546cdc7124d07
Choong <cndqjacndqja@naver.com>
2020-08-27 20:04:31 +0900

    [feat] 홈 뷰 추가

---
+++ app/src/main/java/com/caredirection/cadi/home/HomeFragmentBitamin.kt
@@ -0,0 +1,32 @@
+package com.caredirection.cadi.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.caredirection.cadi.R
+import com.caredirection.cadi.adapter.ChartAdapter
+import com.caredirection.cadi.adapter.ChartData
+import kotlinx.android.synthetic.main.view_pager_home_chart.*
+
+class HomeFragmentBitamin: Fragment(R.layout.view_pager_home_chart) {
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        rvNameSetting()
+    }
+    fun rvNameSetting(){
+        val chartAdapter = ChartAdapter(requireContext())
+
+
+        chartAdapter.items.add( ChartData( "비타민",  200))
+        chartAdapter.items.add( ChartData( "비타민",  50))
+        chartAdapter.items.add( ChartData( "비타민",  600))
+        chartAdapter.items.add( ChartData( "비타민",  60))
+        chartAdapter.items.add( ChartData( "비타민",  20))
+        chartAdapter.items.add( ChartData("비타민", 600))
+        chartAdapter.items.add( ChartData("비타민", 600))
+        chartAdapter.items.add( ChartData("비타민", 600))
+
+        rv_view_pager_home_chart.adapter = chartAdapter
+
+    }
+}
