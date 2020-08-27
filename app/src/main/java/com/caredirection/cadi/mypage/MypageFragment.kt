048737c59465c3d3355bcdbbbc20bfaccc20ac44
Haeeul <etoilune00@gmail.com>
2020-08-28 04:49:29 +0900

    [style] 마이페이지 레이아웃 id 설정

--- app/src/main/java/com/caredirection/cadi/mypage/MypageFragment.kt
+++ app/src/main/java/com/caredirection/cadi/mypage/MypageFragment.kt
@@ -1,7 +1,28 @@
 package com.caredirection.cadi.mypage
 
+import android.content.Intent
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.caredirection.cadi.R
+import com.caredirection.cadi.mypage.interest.MypageInterestProductActivity
+import kotlinx.android.synthetic.main.fragment_mypage.*
 
 class MypageFragment : Fragment(R.layout.fragment_mypage) {
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        makeListener()
+    }
+
+    private fun makeListener(){
+        setInterestClickListener()
+    }
+
+    private fun setInterestClickListener(){
+        btn_mypage_interest_product.setOnClickListener {
+            val interestIntent = Intent(context, MypageInterestProductActivity::class.java)
+
+            startActivity(interestIntent)
+        }
+    }
 }
