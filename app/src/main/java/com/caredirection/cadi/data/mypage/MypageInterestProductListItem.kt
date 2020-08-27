3d4964121a2deda3be89656d4fd0b7de3e80139d
Haeeul <etoilune00@gmail.com>
2020-08-28 04:45:35 +0900

    [feat] 마이페이지 관심제품 레이아웃 구현

---
+++ app/src/main/java/com/caredirection/cadi/data/mypage/MypageInterestProductListItem.kt
@@ -0,0 +1,8 @@
+package com.caredirection.cadi.data.mypage
+
+data class MypageInterestProductListItem(
+    val brand : String,
+    val name : String,
+    val overseas : String,
+    val day : Int
+)
---
+++ app/src/main/java/com/caredirection/cadi/mypage/interest/MypageInterestProductActivity.kt
@@ -0,0 +1,64 @@
+package com.caredirection.cadi.mypage.interest
+
+import android.content.Context
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import com.caredirection.cadi.R
+import com.caredirection.cadi.data.mypage.DummyMypageInterestProductList
+import kotlinx.android.synthetic.main.activity_mypage_interest_product.*
+
+class MypageInterestProductActivity : AppCompatActivity() {
+
+    private lateinit var mypageInterestProductAdapter: MypageInterestProductAdapter
+    private var dummyMypageInterestProductList = DummyMypageInterestProductList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mypage_interest_product)
+
+        setStatusBarTransparent()
+
+        makeListener()
+        interestListInit()
+    }
+
+    private fun interestListInit(){
+        mypageInterestProductAdapter = MypageInterestProductAdapter(this)
+
+        rv_mypage_interest_list.adapter = mypageInterestProductAdapter
+
+        rv_mypage_interest_list.layoutManager = GridLayoutManager(this,2)
+
+        mypageInterestProductAdapter.data = dummyMypageInterestProductList.getMypageInterestProductList()
+
+        mypageInterestProductAdapter.notifyDataSetChanged()
+
+        txt_mypage_interest_count.text = mypageInterestProductAdapter.itemCount.toString()
+    }
+
+    private fun makeListener(){
+        setBackClickListener()
+    }
+
+    private fun setBackClickListener(){
+        btn_mypage_interest_back.setOnClickListener {
+            finish()
+        }
+    }
+
+    // 상태바 투명 설정
+    private fun setStatusBarTransparent(){
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        cl_mypage_interest_product.setPadding(0, getStatusBarHeight(this), 0, 0)
+    }
+
+    // 상태바 높이 정보
+    private fun getStatusBarHeight(context: Context): Int {
+        val resourceId = context.resources.getIdentifier("status_bar_height", "dimen", "android")
+
+        return if (resourceId > 0) context.resources.getDimensionPixelSize(resourceId)
+        else 0
+    }
+}
