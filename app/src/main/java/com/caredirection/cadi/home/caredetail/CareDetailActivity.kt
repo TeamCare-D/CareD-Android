cc99f577a1131b7c76ae5071dff493988c44a468
Choong <cndqjacndqja@naver.com>
2020-08-27 16:31:06 +0900

    [feat] 홈 케어 디테일 뷰 비슷한 케어 추가

--- app/src/main/java/com/caredirection/cadi/home/caredetail/CareDetailActivity.kt
+++ app/src/main/java/com/caredirection/cadi/home/caredetail/CareDetailActivity.kt
@@ -6,6 +6,8 @@ import android.view.Gravity
 import androidx.appcompat.app.AppCompatActivity
 import com.caredirection.cadi.R
 import com.caredirection.cadi.home.caredetail.adapter.CareRvAdapter
+import com.caredirection.cadi.home.caredetail.adapter.CareSimliarData
+import com.caredirection.cadi.home.caredetail.adapter.CareSimliarRvAdapter
 import com.caredirection.cadi.product.list.ListActivity
 import com.github.rubensousa.gravitysnaphelper.GravitySnapHelper
 import kotlinx.android.synthetic.main.activity_home_care_detail.*
@@ -23,9 +25,23 @@ class CareDetailActivity: AppCompatActivity() {
         careRvAdapter.items.add("test")
 
 
-
-
-
         rv_home_care_detail.adapter = careRvAdapter
+
+
+        SimilarRvSetting()
+
+    }
+
+    fun SimilarRvSetting(){
+        val careSimliarRvAdapter =CareSimliarRvAdapter()
+
+        var test = mutableListOf<String>()
+        test.add("진세노사이드")
+        test.add("진세노사이드")
+        test.add("진세노사이드")
+
+        careSimliarRvAdapter.items.add(CareSimliarData("간 건강", test))
+        rv_home_care_detail_similar.adapter = careSimliarRvAdapter
+
     }
 }
--- app/src/main/java/com/caredirection/cadi/home/caredetail/FragmentChartBitamin.kt
+++ app/src/main/java/com/caredirection/cadi/home/caredetail/FragmentChartBitamin.kt
@@ -2,11 +2,7 @@ package com.caredirection.cadi.home.caredetail
 
 import android.os.Bundle
 import android.util.Log
-import android.view.Gravity
-import android.view.View
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SnapHelper
@@ -15,10 +11,9 @@ import com.caredirection.cadi.adapter.ChartAdapter
 import com.caredirection.cadi.adapter.ChartData
 import com.caredirection.cadi.custom.OnSnapPositionChangeListener
 import com.caredirection.cadi.custom.getSnapPosition
-import com.github.rubensousa.gravitysnaphelper.GravitySnapHelper
-import kotlinx.android.synthetic.main.fragment_home_care_detail_chart_bitamin.*
+import kotlinx.android.synthetic.main.fragment_home_care_detail_chart.*
 
-class FragmentChartBitamin: Fragment(R.layout.fragment_home_care_detail_chart_bitamin) {
+class FragmentChartBitamin: Fragment(R.layout.fragment_home_care_detail_chart) {
     lateinit var chartRvADapter: ChartAdapter
 
 
@@ -27,7 +22,73 @@ class FragmentChartBitamin: Fragment(R.layout.fragment_home_care_detail_chart_bi
         super.onActivityCreated(savedInstanceState)
 
 
+        ChartSetting()
+
+        ChartDataSetting()
+
+    }
+
+    inner class SnapOnScrollListener(
+        private val snapHelper: SnapHelper,
+        var behavior: Behavior = Behavior.NOTIFY_ON_SCROLL,
+        var onSnapPositionChangeListener: OnSnapPositionChangeListener? = null
+    ) : RecyclerView.OnScrollListener() {
+
+        private var snapPosition = RecyclerView.NO_POSITION
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            if (behavior == Behavior.NOTIFY_ON_SCROLL) {
+                maybeNotifySnapPositionChange(recyclerView)
+            }
+        }
+
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            if (behavior == Behavior.NOTIFY_ON_SCROLL_STATE_IDLE
+                && newState == RecyclerView.SCROLL_STATE_IDLE) {
+                maybeNotifySnapPositionChange(recyclerView)
+                Log.d("승희 테스트",chartRvADapter.items[snapHelper.getSnapPosition(rv_home_care_detail)].toString())
+                box_home_care_detail_chart.text = chartRvADapter.items[snapHelper.getSnapPosition(rv_home_care_detail)].toString()
+            }
+        }
+
+        private fun maybeNotifySnapPositionChange(recyclerView: RecyclerView) {
+            val snapPosition = snapHelper.getSnapPosition(recyclerView)
+            val snapPositionChanged = this.snapPosition != snapPosition
+            if (snapPositionChanged) {
+                onSnapPositionChangeListener?.onSnapPositionChange(snapPosition)
+                this.snapPosition = snapPosition
+            }
+        }
+
+    }
+
+
+    fun ChartSetting(){
+        val snapHelper = LinearSnapHelper()
+
         chartRvADapter = ChartAdapter(requireContext())
+
+        snapHelper.attachToRecyclerView(rv_home_care_detail)
+        val layoutManager = rv_home_care_detail.layoutManager
+        val snapView = snapHelper.findSnapView(layoutManager)
+
+        val snapPosition = snapView?.let { layoutManager?.getPosition(it) }
+
+        val snapOnScrollListener = SnapOnScrollListener(snapHelper, behavior = Behavior.NOTIFY_ON_SCROLL_STATE_IDLE)
+
+        rv_home_care_detail.addOnScrollListener(snapOnScrollListener)
+
+
+
+        rv_home_care_detail.adapter = chartRvADapter
+
+
+
+        snapHelper.attachToRecyclerView(rv_home_care_detail)
+
+    }
+
+    fun ChartDataSetting(){
         chartRvADapter.items.add(ChartData("",0))
         chartRvADapter.items.add(ChartData("",0))
         chartRvADapter.items.add(ChartData("",0))
@@ -46,79 +107,7 @@ class FragmentChartBitamin: Fragment(R.layout.fragment_home_care_detail_chart_bi
         chartRvADapter.items.add(ChartData("비타민11", 40))
         chartRvADapter.items.add(ChartData("비타민12", 40))
         chartRvADapter.items.add(ChartData("비타민13", 40))
-
-
-
-
-        val snapHelper = LinearSnapHelper()
-
-
-
-        snapHelper.attachToRecyclerView(rv_home_care_detail_bitamin)
-        val layoutManager = rv_home_care_detail_bitamin.layoutManager
-        val snapView = snapHelper.findSnapView(layoutManager)
-
-        val snapPosition = snapView?.let { layoutManager?.getPosition(it) }
-
-        val snapOnScrollListener = SnapOnScrollListener(snapHelper, behavior = Behavior.NOTIFY_ON_SCROLL_STATE_IDLE)
-
-        rv_home_care_detail_bitamin.addOnScrollListener(snapOnScrollListener)
-
-
-
-        rv_home_care_detail_bitamin.adapter = chartRvADapter
-
-
-
-        snapHelper.attachToRecyclerView(rv_home_care_detail_bitamin)
-
-
-
-
-
-
-
     }
-
-    inner class SnapOnScrollListener(
-        private val snapHelper: SnapHelper,
-        var behavior: Behavior = Behavior.NOTIFY_ON_SCROLL,
-        var onSnapPositionChangeListener: OnSnapPositionChangeListener? = null
-    ) : RecyclerView.OnScrollListener() {
-
-
-        private var snapPosition = RecyclerView.NO_POSITION
-
-        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-            if (behavior == Behavior.NOTIFY_ON_SCROLL) {
-                maybeNotifySnapPositionChange(recyclerView)
-            }
-        }
-
-        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-            if (behavior == Behavior.NOTIFY_ON_SCROLL_STATE_IDLE
-                && newState == RecyclerView.SCROLL_STATE_IDLE) {
-                maybeNotifySnapPositionChange(recyclerView)
-                Log.d("승희 테스트",chartRvADapter.items[snapHelper.getSnapPosition(rv_home_care_detail_bitamin)].toString())
-                test.text = chartRvADapter.items[snapHelper.getSnapPosition(rv_home_care_detail_bitamin)].toString()
-            }
-        }
-
-        private fun maybeNotifySnapPositionChange(recyclerView: RecyclerView) {
-            val snapPosition = snapHelper.getSnapPosition(recyclerView)
-            val snapPositionChanged = this.snapPosition != snapPosition
-            if (snapPositionChanged) {
-                onSnapPositionChangeListener?.onSnapPositionChange(snapPosition)
-                this.snapPosition = snapPosition
-            }
-        }
-
-
-
-
-
-    }
-
 }
 enum class Behavior {
     NOTIFY_ON_SCROLL,
