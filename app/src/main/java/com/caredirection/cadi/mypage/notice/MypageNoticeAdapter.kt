9646c1107c539931a27c76f8481e221f4058a500
Haeeul <etoilune00@gmail.com>
2020-09-02 04:45:06 +0900

    [feat] 마이페이지 공지사항 인터랙션 추가

--- app/src/main/java/com/caredirection/cadi/mypage/notice/MypageNoticeAdapter.kt
+++ app/src/main/java/com/caredirection/cadi/mypage/notice/MypageNoticeAdapter.kt
@@ -1,14 +1,21 @@
 package com.caredirection.cadi.mypage.notice
 
 import android.content.Context
+import android.util.SparseBooleanArray
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.caredirection.cadi.R
 import com.caredirection.cadi.data.mypage.MypageNoticeListItem
 
 class MypageNoticeAdapter (private val context: Context) : RecyclerView.Adapter<MypageNoticeViewHolder>(){
     var data: List<MypageNoticeListItem> = listOf()
+
+    private var selectedItem = SparseBooleanArray()
+    private var noticePosition = -1
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MypageNoticeViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.rv_item_mypage_notice, parent, false)
@@ -23,6 +30,23 @@ class MypageNoticeAdapter (private val context: Context) : RecyclerView.Adapter<
     }
 
     override fun onBindViewHolder(holder: MypageNoticeViewHolder, position: Int) {
-        holder.onBind(data[position])
+        holder.onBind(data[position], selectedItem.get(position))
+
+        holder.itemView.setOnClickListener {
+            if(selectedItem.get(position)){
+                selectedItem.delete(position)
+            }
+            else{
+                selectedItem.delete(noticePosition)
+                selectedItem.put(position, true)
+            }
+
+            if(noticePosition != -1){
+                notifyItemChanged(noticePosition)
+            }
+            notifyItemChanged(position)
+
+            noticePosition = position
+        }
     }
 }
--- app/src/main/java/com/caredirection/cadi/mypage/notice/MypageNoticeViewHolder.kt
+++ app/src/main/java/com/caredirection/cadi/mypage/notice/MypageNoticeViewHolder.kt
@@ -1,6 +1,7 @@
 package com.caredirection.cadi.mypage.notice
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.caredirection.cadi.R
@@ -10,9 +11,18 @@ class MypageNoticeViewHolder (view: View) : RecyclerView.ViewHolder(view){
 
     private val txtNoticeDay : TextView = view.findViewById(R.id.txt_mypage_notice_day)
     private val txtNoticeTitle : TextView = view.findViewById(R.id.txt_mypage_notice_title)
+    private val imgNoticeBackGround : ImageView = view.findViewById(R.id.img_mypage_notice_background)
+    private val txtNoticeContent : TextView = view.findViewById(R.id.txt_mypage_notice_content)
 
-    fun onBind(notice: MypageNoticeListItem){
+    fun onBind(notice: MypageNoticeListItem, isExpanded: Boolean){
         txtNoticeDay.text = notice.day
         txtNoticeTitle.text = notice.title
+
+        changeVisibility(isExpanded)
+    }
+
+    private fun changeVisibility(isExpanded : Boolean){
+        imgNoticeBackGround.visibility = if(isExpanded) View.VISIBLE else View.GONE
+        txtNoticeContent.visibility = if(isExpanded) View.VISIBLE else View.GONE
     }
 }
