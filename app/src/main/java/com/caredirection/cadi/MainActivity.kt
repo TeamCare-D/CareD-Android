31f525bc91124a6eb2412c41a474da03ee342ad1
Haeeul <etoilune00@gmail.com>
2020-09-08 05:16:27 +0900

    [style] 상태바 영역 수정

--- app/src/main/java/com/caredirection/cadi/MainActivity.kt
+++ app/src/main/java/com/caredirection/cadi/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.caredirection.cadi
 
-import android.content.Context
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,15 +28,6 @@ class MainActivity : AppCompatActivity() {
     // 상태바 투명 설정
     private fun setStatusBarTransparent(){
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
-        cl_main.setPadding(0, getStatusBarHeight(this), 0, 0)
-    }
-
-    // 상태바 높이 정보
-    private fun getStatusBarHeight(context: Context): Int {
-        val resourceId = context.resources.getIdentifier("status_bar_height", "dimen", "android")
-
-        return if (resourceId > 0) context.resources.getDimensionPixelSize(resourceId)
-        else 0
     }
 
 }
