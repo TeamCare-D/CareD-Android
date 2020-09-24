8395fae09e42638d411c48f5ad8099f47cddc840
Haeeul <etoilune00@gmail.com>
2020-09-24 23:18:54 +0900

    [style] 나만의 복용제품등록 성분고르기 다이얼로그 아이템 클릭리스너 수정

--- app/src/main/java/com/caredirection/cadi/register/user/ingredient/vitamin/RegisterVitaminAdapter.kt
+++ app/src/main/java/com/caredirection/cadi/register/user/ingredient/vitamin/RegisterVitaminAdapter.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.caredirection.cadi.R
+import com.caredirection.cadi.data.register.IngredientSelectList
 import com.caredirection.cadi.data.register.RvIngredientListItem
+import kotlinx.android.synthetic.main.rv_item_register_vitamin.view.*
 
 class RegisterVitaminAdapter (private val context: Context?) : RecyclerView.Adapter<RegisterVitaminViewHolder>(){
+
     var data: List<RvIngredientListItem> = listOf()
+    var selectedItem = IngredientSelectList.selectedVitaminList
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RegisterVitaminViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.rv_item_register_vitamin, parent, false)
@@ -24,5 +28,36 @@ class RegisterVitaminAdapter (private val context: Context?) : RecyclerView.Adap
 
     override fun onBindViewHolder(holder: RegisterVitaminViewHolder, position: Int) {
         holder.onBind(data[position])
+
+        setItemBackground(holder, position)
+
+        holder.itemView.setOnClickListener {
+            toggleItemSelected(position)
+        }
+    }
+
+    private fun setItemBackground(holder: RegisterVitaminViewHolder, position: Int){
+        if(isItemSelected(position)){
+            holder.itemView.btn_register_vitamin_check.background = context!!.resources.getDrawable(R.drawable.img_selected)
+        }
+        else{
+            holder.itemView.btn_register_vitamin_check.background = context!!.resources.getDrawable(R.drawable.img_empty)
+        }
+
+    }
+
+    private fun isItemSelected(position: Int): Boolean{
+        return selectedItem.contains(data[position].name)
+    }
+
+    private fun toggleItemSelected(position: Int) {
+        if (selectedItem.contains(data[position].name)) {
+            selectedItem.remove(data[position].name)
+        }
+        else {
+            selectedItem.add(data[position].name)
+        }
+
+        notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/caredirection/cadi/register/user/ingredient/vitamin/RegisterVitaminViewHolder.kt
+++ app/src/main/java/com/caredirection/cadi/register/user/ingredient/vitamin/RegisterVitaminViewHolder.kt
@@ -1,7 +1,6 @@
 package com.caredirection.cadi.register.user.ingredient.vitamin
 
 import android.view.View
-import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.caredirection.cadi.R
@@ -9,13 +8,8 @@ import com.caredirection.cadi.data.register.RvIngredientListItem
 
 class RegisterVitaminViewHolder(view: View) : RecyclerView.ViewHolder(view){
     private val txtVitaminName : TextView = view.findViewById(R.id.txt_register_vitamin_name)
-    private val btnVitaminCheck : CheckBox = view.findViewById(R.id.btn_register_vitamin_check)
 
     fun onBind(vitamin : RvIngredientListItem){
         txtVitaminName.text = vitamin.name
-
-        itemView.setOnClickListener {
-            btnVitaminCheck.isChecked = !btnVitaminCheck.isChecked
-        }
     }
 }
