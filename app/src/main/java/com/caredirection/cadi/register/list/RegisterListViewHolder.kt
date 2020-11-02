e57059325f38fd56d82de4e99a000225ef77b361
Haeeul <etoilune00@gmail.com>
2020-11-03 08:21:03 +0900

    [refactor] 복용제품리스트 이미지 추가

--- app/src/main/java/com/caredirection/cadi/register/list/RegisterListViewHolder.kt
+++ app/src/main/java/com/caredirection/cadi/register/list/RegisterListViewHolder.kt
@@ -1,17 +1,20 @@
 package com.caredirection.cadi.register.list
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.caredirection.cadi.R
 import com.caredirection.cadi.data.register.RvTakeListItem
 
-class RegisterListViewHolder(view: View) : RecyclerView.ViewHolder(view){
+class RegisterListViewHolder(private val view: View) : RecyclerView.ViewHolder(view){
 
     private val txtProductBrand : TextView = view.findViewById(R.id.txt_register_list_brand)
     private val txtProductName : TextView = view.findViewById(R.id.txt_register_list_name)
     private val txtProductOverseas : TextView = view.findViewById(R.id.txt_register_list_overseas)
     private val txtProductDay : TextView = view.findViewById(R.id.txt_register_list_day)
+    private val imgProductUrl : ImageView = view.findViewById(R.id.img_register_list)
 
     fun onBind(product: RvTakeListItem){
         txtProductBrand.text = product.brand
@@ -22,9 +25,9 @@ class RegisterListViewHolder(view: View) : RecyclerView.ViewHolder(view){
             txtProductOverseas.text = "해외직구"
         }
 
-//        Glide.with(this@MypageInterestProductActivity)
-//            .load(detailsReso.main_contents.image_key)
-//            .centerCrop()
-//            .into(img_article_details)
+        Glide.with(view)
+            .load(product.imgUrl)
+            .centerCrop()
+            .into(imgProductUrl)
     }
 }
